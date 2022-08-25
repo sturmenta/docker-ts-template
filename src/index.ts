17d903d9258837c6203d7296ae72f007e43edeae
sturmenta <sturmenta@gmail.com>
2022-08-24 21:12:36 -0300

    basic ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
-const bodyParser = require("body-parser");
-const app = require("express")();
+import bodyParser from "body-parser";
+import express, { Request, Response } from "express";
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/test", async (req, res) => {
-  const myPass = process.env.MY_SUPER_PASSWORD;
-
-  res.json({ result: "test working", myPass });
+app.get("/", async (req: Request, res: Response) => {
+  return res.json({ status: "success" });
 });
 
 // ────────────────────────────────────────────────────────────────────────────────
 
-app.listen(process.env.port || 3000, () => {
-  console.log(`Server running on port ${process.env.port || 3000}`);
-});
+app.listen(3000, () => console.log(`Server running on port 3000`));
